ab1985842b210db4581e0969be1d5dcfd7db5ae9
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2020-07-11 23:04:57 +0100

    Change template librarry to sailfish

--- src/main.rs
+++ src/main.rs
@@ -6,140 +6,102 @@ use std::{
 };
 
 #[macro_use]
+extern crate sailfish_macros;
+#[macro_use]
 extern crate serde_derive;
 
-use actix_web::client::Client;
-use actix_web::{error as AnotherError, http, middleware, web, App, Error as ActixError, HttpResponse, HttpServer};
-use clap::{value_t, Arg};
+use actix_web::{web, middleware, App, error::InternalError, http::StatusCode, HttpResponse, HttpServer};
+use dotenv::dotenv;
+use once_cell::sync::Lazy;
 use rand::Rng;
+use sailfish::TemplateOnce;
 use sitemap::{structs::Location, reader::{SiteMapEntity, SiteMapReader}};
-use once_cell::sync::OnceCell;
-
 use tantivy::{
     collector::{Count, TopDocs},
     query::QueryParser,
     schema::{Field, FieldType, NamedFieldDocument, Schema, Value},
     tokenizer::*,
-    DocAddress, Document, Index, IndexReader, Score,
+    DocAddress, Document, Index as TantivyIndex, IndexReader, Score,
 };
-use tera::Tera;
 
-// store tera template in application state
-async fn index(
-    tmpl: web::Data<tera::Tera>,
-    query: web::Query<HashMap<String, String>>,
-    tantivy_index: web::Data<PathBuf>
-) -> Result<HttpResponse, ActixError> {
-    let mut ctx = tera::Context::new();
-    let s = if let Some(query) = query.get("q") {
-        // submitted form
-        let pages = get_search_results(&query, tantivy_index);
-        if pages.is_empty() {
-            ctx.insert("query", &query);
-            tmpl.render("noresults.html", &ctx)
-                .map_err(|_| AnotherError::ErrorInternalServerError("Template error for noresults"))?
-        } else {
-            ctx.insert("query", &query);
-            ctx.insert("pages", &pages);
-            tmpl.render("search.html", &ctx)
-                .map_err(|_| AnotherError::ErrorInternalServerError("Template error for search"))?
-        }
-    } else {
-        tmpl.render("index.html", &ctx)
-            .map_err(|_| AnotherError::ErrorInternalServerError("Template error for index"))?
-    };
-    Ok(HttpResponse::Ok().content_type("text/html").body(s))
-}
+static ADDR: Lazy<String> = Lazy::new (||{
+    std::env::var("ADDR").expect("ADDR must be set")
+});
 
-async fn random(site_map: web::Data<Vec<Location>>) -> HttpResponse {
-    HttpResponse::Found()
-        .header(
-            http::header::LOCATION,
-            site_map[rand::thread_rng().gen_range(0, site_map.len())].get_url().unwrap().as_str(),
-        )
-        .finish()
-        .into_body()
-}
+static PORT: Lazy<u16> = Lazy::new (||{
+    std::env::var("PORT").expect("PORT must be set").parse::<u16>().unwrap()
+});
 
-
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    let matches = clap::App::new("HTTP Proxy")
-        .arg(
-            Arg::with_name("listen_addr")
-                .takes_value(true)
-                .value_name("LISTEN ADDR")
-                .index(1)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("listen_port")
-                .takes_value(true)
-                .value_name("LISTEN PORT")
-                .index(2)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("tera_templates")
-                .takes_value(true)
-                .value_name("PATH TO TERA TEMPLATES")
-                .index(3)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("tantivy_index")
-                .takes_value(true)
-                .value_name("PATH TO TANTIVY INDEX")
-                .index(4)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("sitemap")
-                .takes_value(true)
-                .value_name("PATH TO SITEMAP")
-                .index(5)
-                .required(true),
-        )
-        .get_matches();
-
-    let listen_addr = matches.value_of("listen_addr").unwrap();
-    let listen_port = value_t!(matches, "listen_port", u16).unwrap_or_else(|e| e.exit());
-
-    let tera_templates = value_t!(matches, "tera_templates", PathBuf).unwrap_or_else(|e| e.exit());
-    let tera = Tera::new(&(tera_templates.as_os_str().to_str().unwrap().to_string() + "/**/*")).unwrap();
-    let tantivy_index = value_t!(matches, "tantivy_index", PathBuf).unwrap_or_else(|e| e.exit());
-    //let sitemap = value_t!(matches, "sitemap", String).unwrap_or_else(|e| e.exit());
-
-    static SITEMAP: OnceCell<Vec<Location>> = OnceCell::new();
-    SITEMAP.get_or_init(|| {
-        let sitemap = value_t!(matches, "sitemap", String).unwrap_or_else(|e| e.exit());
+static SITEMAP: Lazy<Vec<Location>> = Lazy::new (||{
         let mut urls = Vec::new();
-        for entity in SiteMapReader::new(File::open(Path::new(&sitemap)).unwrap()) {
+        for entity in SiteMapReader::new(File::open(Path::new(&std::env::var("SITEMAP").expect("SITEMAP must be set"))).unwrap()) {
             if let SiteMapEntity::Url(url_entry) = entity {
                 urls.push(url_entry.loc);
             }
         }
         urls
-    });
+});
 
+static TANTIVY_INDEX: Lazy<String> = Lazy::new (||{
+    std::env::var("TANTIVY_INDEX").expect("TANTIVY_INDEX must be set")
+});
+
+#[derive(TemplateOnce)]
+#[template(path = "search.stpl")]
+struct Search<'a> {
+    query: &'a str,
+    pages: Vec<Page>,
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "index.stpl")]
+struct Index;
+
+async fn index(query: web::Query<HashMap<String, String>>) -> actix_web::Result<HttpResponse> {
+    let body = if let Some(query) = query.get("q") {
+        let pages = get_search_results(&query);
+        Search { query, pages }
+        .render_once()
+        .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?
+        } else {
+        Index
+        .render_once()
+        .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?
+    };
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(body))
+}
+
+async fn random() -> actix_web::Result<HttpResponse> {
+    Ok(HttpResponse::Found()
+        .header(
+            actix_web::http::header::LOCATION,
+            SITEMAP[rand::thread_rng().gen_range(0, SITEMAP.len())].get_url().unwrap().as_str(),
+        )
+        .finish()
+        .into_body())
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    
     HttpServer::new(move || {
         App::new()
-            .data(Client::new())
-            .data(tera.clone())
-            .data(tantivy_index.clone())
-            .data(SITEMAP.clone())
             .wrap(middleware::Logger::default())
             .service(web::resource("/").route(web::get().to(index)))
             .service(web::resource("/rando").route(web::get().to(random)))
     })
-    .bind((listen_addr, listen_port))?
-    .system_exit()
+    .bind((ADDR.as_str(), *PORT))?
     .run()
     .await
 }
 
-fn get_search_results(query: &str, tantivy_index: web::Data<PathBuf>) -> Vec<Page> {
-    let index_directory = PathBuf::from(tantivy_index.get_ref());
+
+fn get_search_results(query: &str) -> Vec<Page> {
+    let index_directory = PathBuf::from(TANTIVY_INDEX.as_str());
     let index_server = IndexServer::load(&index_directory);
     let serp = index_server.search(query.to_string(), 100).unwrap();
     let scheme = serp.schema;
@@ -221,7 +183,7 @@ struct IndexServer {
 
 impl IndexServer {
     fn load(path: &Path) -> IndexServer {
-        let index = Index::open_in_dir(path).unwrap();
+        let index = TantivyIndex::open_in_dir(path).unwrap();
         index.tokenizers().register(
             "commoncrawl",
             TextAnalyzer::from(SimpleTokenizer)
